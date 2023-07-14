268d12d591187c51f720958d6c859cac696c1901
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-07-13 20:21:32 -1200

    Add a hambuger menu for mobile sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import  { useEffect, useState } from 'react'
 import logo from "../images/logo.png"
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import {getAuth, onAuthStateChanged} from  "firebase/auth";
+import {FaBars} from "react-icons/fa"
+import {FaTimes} from "react-icons/fa"
 
 
 export default function Header() {
   const [pageState, setPageState] = useState("Sign in")
   const navigate = useNavigate()
   const location = useLocation()
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav);
   const auth = getAuth();
   useEffect(()=>{
     onAuthStateChanged(auth, (user)=>{
@@ -31,7 +35,7 @@ export default function Header() {
           ("/")}/>
         </div>
         <div>
-          <ul className='flex space-x-10'>
+          <ul className='flex space-x-10 hidden md:flex'>
             <li className={`py-3 text-sm font-semibold text-gray-300 border-b-[3px] border-b-transparent cursor-pointer ${pathMatchRoute("/") && "!text-black !border-b-green-500"}`}  onClick={()=>navigate
           ("/")}>Home</li>
                       <li className={`py-3 text-sm font-semibold text-gray-300 border-b-[3px] border-b-transparent cursor-pointer ${pathMatchRoute("/find-hospital") && "!text-black !border-b-green-500"}`}  onClick={()=>navigate
@@ -41,6 +45,24 @@ export default function Header() {
             ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "!text-black !border-b-green-500"}`}  
             onClick={()=>navigate
           ("/profile")}>{pageState}</li>
+          </ul>
+        </div>
+
+        {/* Handbugger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+          {!nav ? <FaBars/> : <FaTimes/>}
+
+          {/* Mobile menu */}
+          <ul className={
+            !nav ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen  flex flex-col justify-center items-center"
+          }>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="/" >Home</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="find-hospital">Find Hospital</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="sign-in">Sign In</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="sign-up">Sign Up</Link></li>
+
+
           </ul>
         </div>
       </header>
